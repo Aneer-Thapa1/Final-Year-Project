7d3ffca751249d2893361d553a15420cb448feff
Anir Jung Thapa <134839446+Aneer-Thapa1@users.noreply.github.com>
2024-12-30 12:26:16 +0545

    auth backend

--- Server/app/users/userController.js
+++ Server/app/users/userController.js
@@ -27,6 +27,9 @@ const signin = async (req, res) => {
         // Hash the password
         const hashedPassword = await bcrypt.hash(password, 10);
 
+
+
+
         // Create a new user
         const newUser = await prisma.user.create({
             data: {
