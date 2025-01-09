75127597785c203e8eae16790903b649114208db
Anir Jung Thapa <134839446+Aneer-Thapa1@users.noreply.github.com>
2025-01-09 21:30:42 +0545

    friensdhip started

--- Server/app/controllers/habitController.js
+++ Server/app/controllers/habitController.js
@@ -1,12 +1,7 @@
 import res from "express/lib/response";
 import req from "express/lib/request";
 
+const { PrismaClient } = require("@prisma/client");
 const prisma = new PrismaClient();
 
-const {PrismaClient} = require('@prisma/client');
-const prisma = new PrismaClient();
-
-
-const addHabit = async (req, res) => {
-    
-}
+const addHabit = async (req, res) => {};
