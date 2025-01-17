fa8f019d4cb61c60c4dbc26b4a0a45f5ebaa47ee
Anir Jung Thapa <134839446+Aneer-Thapa1@users.noreply.github.com>
2025-01-17 21:36:31 +0545

    add habit started

--- Server/app/controllers/friendshipController.js
+++ Server/app/controllers/friendshipController.js
@@ -5,4 +5,10 @@ const prisma = new PrismaClient();
 const addFriend = async (req, res) => {
     const {user_id } = req.body
     const sender_id = req.user.id
+
+    const sender = await prisma.user.findFirst(sender_id)
+
+    const reciver = await prisma.user.findFirst(user_id)
+
+
 }
